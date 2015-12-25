e155f4035656ee0a303723e02f6dcb1b75957bc9
MoiRouhs <carsrouhs@gmail.com>
2015-12-24 22:13:39 -0500

    Añadidos todos los numeros y manifest FX OS

--- js/chiffres.js
+++ js/chiffres.js
@@ -1,8 +1,9 @@
-function digito(nombre, fonetique, audio){
+function digito(nombre, fonetique, clase, audio){
     var numero = document.getElementById("h1js");
     var fonetica = document.getElementById("h3js");
     var au = document.getElementById(audio);
         numero.innerHTML=nombre;
+        numero.className=clase;
         fonetica.innerHTML= fonetique;
         au.play();
-}
+};
