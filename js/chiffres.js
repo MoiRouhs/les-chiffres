12a0e824153f5f71404a625308aecb46d750f1ff
MoiRouhs <carsrouhs@gmail.com>
2016-06-04 01:15:08 -0500

    trbaajando ejercicios

--- js/chiffres.js
+++ js/chiffres.js
@@ -11,63 +11,73 @@ function digito(nombre, fonetique, clase, audio){
 // Esta función es para mostrar los ejercicios aleatoriamente 
 var antes = null;
     var contar= 0;
-    var ejercicios = ["e1","e2","e3","e4","e5","e6","e7","e8","e9","e10","e11","e12","e13","e14","e15","e16","e17","e18","e19","e20","e21","e22","e23","e24","e25","e26","e27","e28","e29","e30"];
-
-    function comenzar(){
-    var numero = Math.round(Math.random()*((ejercicios.length)-1));
-    var id = ejercicios[numero];
-    var visible = document.getElementById(id);
-    var inicio = document.getElementById("inicio")
-        
-        inicio.className="caja invisible";
-        visible.className="caja";
-        contar++;
-        console.log(numero,id,contar)
-        
+    var contar= 0;
+    var hechos=[];
+    var numero= 0 ;
+    var negativa = 0;
+    var ejercicios=[1,2,3];
+    var respuestas=[1,2,3];
+    
+    function comenzar(id,preg){
+        numero = Math.round(Math.random()*((preg.length)-1));
+    var input = document.getElementById("sub");
+    var cuerpo = document.getElementById("e1");
+    var borrar = document.getElementById(id);
+    borrar.style.display = "none";
+    input.style.display ="flex";
+    cuerpo.innerHTML ='<div class="relativo"><div class="centrado"><h1>'+preg[numero]+'</h1></div></div>';
 }
-function azar(){
-    var numero = Math.round(Math.random()*((ejercicios.length)-1));
-    var id = ejercicios[numero];
-//     var insertar= document.getElementById("cantidad");
-    var invisible = document.getElementById(antes);
-    var visible = document.getElementById(id);
-    var fin = document.getElementById("fin");
-        
-        contar ++;
-        insertar.innerHTML=contar;
-        console.log(id, numero,diferente,((ejercicios.length)-1));
-        
-        if (contar <= 10){
-            for (var i = 0; i <= 2; i++) {
-                switch(i){
-                    case 1:
-                        invisible.className="caja invisible";
-                    break;
-                    case 2:
-                        visible.className="caja";
-                        antes = id;
-                    break;
-                }
+function quiz(pregun,respu,event) {
+    
+    var enter = event.keyCode;
+    var input1 = document.getElementById("sub");
+    var puntaje = document.getElementById("puntaje");
+    var error = document.getElementById("errores");
+    var bien = document.getElementById("bien");
+    var mal = document.getElementById("mal");
+    var aparecer = document.getElementById("tot");
+    var estudio = document.getElementById("estudiar");
+    var val = document.getElementById('getexto').value;
+    var cuerpo=document.getElementById("e1");
+    var valor = val.toLowerCase();
+    var respuesta = respu[numero];
+    if(enter == 13 | enter == undefined){
+        if(valor == respuesta){
+            contar++;
+            if(contar <= 14){
+//                 puntaje.innerHTML= contar;
+                numero = Math.round(Math.random()*((pregun.length)-1));
+                console.log(numero,respuesta,pregun.length);
+                cuerpo.innerHTML ='<div id="pr" class="relativo"><div class="centrado"><h1>'+pregun[numero]+'</h1></div></div>';
+                aparecer = true;
+//                 bien.style.display ="inherit";
+//                 setTimeout(function a(){bien.style.display ="none";}, 500);
+                 
+            }
+            if(contar == 15){
+                puntaje.innerHTML= contar;
+                aparecer.style.display="inherit";
+                input1.style.display ="none";
             }
         }else{
-            invisible.className="caja invisible";
-            fin.className="caja";
+            negativa++;
+            switch(negativa){
+                case 1:
+                    error.innerHTML +="X";
+                    mal.style.display ="inherit";
+                    setTimeout(function a(){mal.style.display ="none";}, 500);
+                    break;
+                case 2:
+                    error.innerHTML +="X";
+                    mal.style.display ="inherit";
+                    setTimeout(function a(){mal.style.display ="none";}, 500);
+                    break;
+                case 3:
+                    estudio.style.display="inherit";
+                    input1.style.display ="none";
+                    error.innerHTML +="X";
+                    break;
+            }
         }
-}
-// function para los diferentes ejercicios
-function escrito(id,respuesta,event) {
-    var enter = event.keyCode;
-    var valor = document.getElementById(id).value;
-    
-    console.log(valor, enter)
-    
-    if(enter == 13 | enter == undefined){
-        console.log("oui")
-	if(valor == respuesta){
-            console.log("Bravo");
-        }else{
-            console.log("Mal");
-        };
     }
 }
-
