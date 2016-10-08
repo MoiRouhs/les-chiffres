2f4fe6fda61b3c581e79c6a86747b7a5814330e3
MoiRouhs <carsrouhs@gmail.com>
2016-10-08 03:23:41 -0500

    reorganizando estructura de ejercicios

--- js/chiffres.js
+++ js/chiffres.js
@@ -9,75 +9,40 @@ function digito(nombre, fonetique, clase, audio){
         au.play();
 }
 // Esta función es para mostrar los ejercicios aleatoriamente 
-var antes = null;
-    var contar= 0;
-    var contar= 0;
-    var hechos=[];
-    var numero= 0 ;
-    var negativa = 0;
-    var ejercicios=[1,2,3];
-    var respuestas=[1,2,3];
-    
-    function comenzar(id,preg){
-        numero = Math.round(Math.random()*((preg.length)-1));
-    var input = document.getElementById("sub");
-    var cuerpo = document.getElementById("e1");
-    var borrar = document.getElementById(id);
-    borrar.style.display = "none";
-    input.style.display ="flex";
-    cuerpo.innerHTML ='<div class="relativo"><div class="centrado"><h1>'+preg[numero]+'</h1></div></div>';
+function pregres(num){
+var opciones = Object.keys(ejercicios); 
+var cantidad = (num <= opciones.length & num > 0) ? num : opciones.length ; 
+var numero1 = Math.round(Math.random()*((cantidad)-1));
+var gruporespuestas = opciones[numero1];
+var opcionespregunta = ejercicios[gruporespuestas];
+var eleccion = Object.keys(opcionespregunta);
+var numero2 = Math.round(Math.random()*((eleccion.length)-1));
+var variedadpregunta = eleccion[numero2];
+var seleccionpregunta = ejercicios[gruporespuestas];
+var pregunta1 = seleccionpregunta[variedadpregunta]; 
+var respuesta = {};
+var pregunta= {};
+var ejercicio = {pregunta, respuesta};
+
+for (var exep in exepciones){
+    var exepcion = exepciones[exep];
+    if (exepcion != variedadpregunta){
+        exepciones.push(variedadpregunta);
+    }
 }
-function quiz(pregun,respu,event) {
-    
-    var enter = event.keyCode;
-    var input1 = document.getElementById("sub");
-    var puntaje = document.getElementById("puntaje");
-    var error = document.getElementById("errores");
-    var bien = document.getElementById("bien");
-    var mal = document.getElementById("mal");
-    var aparecer = document.getElementById("tot");
-    var estudio = document.getElementById("estudiar");
-    var val = document.getElementById('getexto').value;
-    var cuerpo=document.getElementById("e1");
-    var valor = val.toLowerCase();
-    var respuesta = respu[numero];
-    if(enter == 13 | enter == undefined){
-        if(valor == respuesta){
-            contar++;
-            if(contar <= 14){
-//                 puntaje.innerHTML= contar;
-                numero = Math.round(Math.random()*((pregun.length)-1));
-                console.log(numero,respuesta,pregun.length);
-                cuerpo.innerHTML ='<div id="pr" class="relativo"><div class="centrado"><h1>'+pregun[numero]+'</h1></div></div>';
-                aparecer = true;
-//                 bien.style.display ="inherit";
-//                 setTimeout(function a(){bien.style.display ="none";}, 500);
-                 
-            }
-            if(contar == 15){
-                puntaje.innerHTML= contar;
-                aparecer.style.display="inherit";
-                input1.style.display ="none";
-            }
-        }else{
-            negativa++;
-            switch(negativa){
-                case 1:
-                    error.innerHTML +="X";
-                    mal.style.display ="inherit";
-                    setTimeout(function a(){mal.style.display ="none";}, 500);
-                    break;
-                case 2:
-                    error.innerHTML +="X";
-                    mal.style.display ="inherit";
-                    setTimeout(function a(){mal.style.display ="none";}, 500);
-                    break;
-                case 3:
-                    estudio.style.display="inherit";
-                    input1.style.display ="none";
-                    error.innerHTML +="X";
-                    break;
-            }
+for (var item in opcionespregunta){
+    for (ite in exepciones){
+         var item2 = exepciones[ite];
+         if (item2 != item){
+            delete opcionespregunta[item2];
         }
     }
 }
+var res = Object.keys(opcionespregunta);
+var numero3 = Math.round(Math.random()*((res.length)-1));
+var respuesta1 = res[numero3];
+var respuesta2 = opcionespregunta[respuesta1];
+respuesta[respuesta1]= respuesta2;
+pregunta[variedadpregunta]= pregunta1;
+return ejercicio
+};
